a0e65343ea44a23579aecbe05a269267636f6923
Krish <kushwahakrish67@gmail.com>
2026-05-21 14:09:05 +0530

    Starting a new project. I'll be trying to make a browser based Sci-Fi OS. I'll be using html css Javascript
    - work in progress -

---
+++ script.js
@@ -0,0 +1,18 @@
+function fullscreen() {
+    document.documentElement.requestFullscreen();
+    document.getElementById('fullscreen_button').remove();
+    let video = document.getElementById('startup_animation')
+    video.style.display = 'Block';
+    video.style.width = '100vw';
+    video.style.height = '100vh';
+    video.style.position = 'Fixed';
+    document.getElementById('startup_container').style.background = 'Black';
+    document.getElementById('welcome').remove();
+    let logo = document.getElementById('logo')
+    logo.style.display = 'Block'
+    document.getElementById('loading').style.display = 'Block';
+    document.getElementById('loading_message').style.display = 'Block';
+    setTimeout(() => {
+    window.location.href = "Loading_screen.html";
+    }, 5000);
+}
